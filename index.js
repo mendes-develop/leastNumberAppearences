bc25e9a12700631d305e0403432a220b53dea609
Alex Mendes <mendes.develop@gmail.com>
2020-01-03 14:57:30 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let arr = ["apple","orange","apple","orange","pear","orange", "pear", "pear", "apple"];
+
+function getCount(array){
+  return array.reduce((prev, next) => {
+    prev[next] = (prev[next] += 1) || 1
+    return prev
+  }, {})
+}
+
+function getLeast(obj){
+  return Math.min(...Object.values(obj))
+}
+
+function getKeysByValue(object, value) {
+  return Object.keys(object).filter(key => object[key] === value);
+}
+
+let count = getCount(arr)
+let leastVal = getLeast(count)
+let repeatLeast = getKeysByValue(count, leastVal)
+
+console.log(count)
+console.log('----------------------------------')
+console.log(leastVal)
+console.log('----------------------------------')
+console.log(repeatLeast)
